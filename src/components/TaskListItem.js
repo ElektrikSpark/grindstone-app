bd9a563985dc39cc1c696e1b880b699fb6883d01
ElektrikSpark <elektrikspark@gmail.com>
2018-05-22 01:51:31 -0500

    further mobile changes and finalized styles

--- src/components/TaskListItem.js
+++ src/components/TaskListItem.js
@@ -6,19 +6,20 @@ import numeral from 'numeral'
 const TaskListItem = ({ id, description, amount, createdAt, deadline }) => (
     <Link className="list-item" to={`/edit/${id}`}>
         <div>
-            <h3 className="list-item__title show-for-desktop">{description}</h3>
+            <h2 className="list-item__title show-for-desktop">{description}</h2>
             <h2 className="list-item__title show-for-mobile">{description}</h2>
             <span className="list-item__subtitle">Date Created: {moment(createdAt).format("MMMM Do, YYYY")}</span>
         </div>
         {deadline != null && <h2 className="list-item__deadline show-for-desktop">{moment(deadline).format("MMMM Do, YYYY")}</h2>}
-        {deadline != null && <h3 className="list-item__deadline show-for-mobile">Deadline: {moment(deadline).format("MMMM Do, YYYY")}</h3>}
+        
         
         <h3 className="list-item__data show-for-desktop">
             {amount === 0 ? 'N/A' : amount} 
         </h3>
         <h3 className="list-item__data show-for-mobile">
-            Hours: {amount === 0 ? 'N/A' : amount} 
+            {amount === 1 ? `${amount} hour` : (amount === 0 ? 'N/A' : `${amount} hours`) }
         </h3>
+        {deadline != null && <h4 className="list-item__deadline show-for-mobile">Deadline: {moment(deadline).format("MMMM Do, YYYY")}</h4>}
     </Link>
 )
 
